af85de2e28ba73859175bc75885136da5763224e
Kristablackey <blackey@hartford.edu>
2025-10-07 13:44:33 -0400

    Update CountFlips.java

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,26 @@
 public class CountFlips {
+//-----------------------------------------------------------
+// Creates a Coin object, flips it, and prints the results.
+//-----------------------------------------------------------
   public static void main(String[] args) {
+    final int FLIPS = 100;
+    int headsCount = 0;
+    int tailsCount = 0;
+
+    Coin myCoin = new Coin();
+
+    for (int i = 0; i < FLIPS; i++){
+      myCoin.flip();
+
+      if (myCoin.isHeads()) {
+        headsCount++;
+      } else {
+        tailsCount++;
+      }
+    }
+
+    System.out.println("After" + FLIPS + "Flips");
+    System.out.println("Heads: " + headsCount);
+    System.out.println("Tails: " + tailsCount);
   }
 }
